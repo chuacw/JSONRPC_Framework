efd04b2d8802bfee818cc99413e7419889fdaa01
chuacw <1757930+chuacw@users.noreply.github.com>
2024-02-17 13:48:36 +0800

    Restored Polkadot Client
    Restored Polkadot Client

---
+++ Web3/Polkadot/JSONRPC.Web3.Polkadot.Types.pas
@@ -0,0 +1,15 @@
+﻿unit JSONRPC.Web3.Polkadot.Types;
+
+interface
+
+type
+  TSystemHealth = record
+    peers: Integer;
+    isSyncing: Boolean;
+    shouldHavePeers: Boolean;
+  end;
+
+implementation
+
+end.
+
